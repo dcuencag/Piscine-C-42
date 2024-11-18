6f094ca863e638b470c58a76970d95284bafa6be
Daniel Cuenca Gutiérrez <daniel@MacBook-Air-de-Daniel.local>
2024-11-18 23:59:55 +0100

    let's go agian

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,48 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: dancuenc <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 17:59:12 by dancuenc          #+#    #+#             */
-/*   Updated: 2024/11/18 18:28:59 by dancuenc         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdio.h>
 
 char	*ft_strcapitalize(char *str)
 {
-	int	i;
-	int	new;
-
-	i = 0;
-	new = 1;
-	while (str[i])
-	{
-		if ((str[i] >= 'a' && str[i] <= 'z')
-			|| (str[i] >= 'A' && str[i] <= 'Z'))
-		{
-			if (new == 1 && str[i] >= 'a')
-				str[i] = str[i] - 32;
-			else if (new == 0 && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-			new = 0;
-		}
-		else
-			new = (str[i] < '0' || str[i] > '9');
-		i++;
-	}
-	return (str);
+    
 }
 
-int	main(void)
+int main(void)
 {
-	char str[] = "qUE nO me ,dIGAS ?qUe !Sto es fácil";
-	
-	printf("\nBefore: %s\n\n", str);
-	ft_strcapitalize(str);
-	printf("After:  %s\n\n", str);
-	return (0);
+    char str[] = "some 1jh 1things are ,ment ¿to"
+
+    ft_strcapitalize(str);
+
 }
