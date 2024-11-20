30bbe7ce481a0ac59fc5b39e881ef77dab2356cc
dcuencag <d@loscuenca.com>
2024-11-20 13:54:51 +0100

    Add files via upload

---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dancuenc <dancuenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 13:17:16 by dancuenc          #+#    #+#             */
+/*   Updated: 2024/11/20 13:41:41 by dancuenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	n = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[n] != '\0')
+	{
+		dest[i] = src[n];
+		i++;
+		n++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/* 
+int	main(void)
+{
+	char s1[] = "1234";
+	char s2[] = "5678";
+
+	ft_strcat(s1, s2);
+	printf("%s\n", s1);
+} */
