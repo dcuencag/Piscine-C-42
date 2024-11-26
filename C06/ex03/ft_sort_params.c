e55ca85c74360b038c0d6f0b5febb2bee0ebc53b
Daniel Cuenca <d@loscuenca.com>
2024-11-26 19:02:00 +0100

    Add files via upload

---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dancuenc <dancuenc@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/25 12:22:31 by dancuenc          #+#    #+#             */
+/*   Updated: 2024/11/26 18:54:43 by dancuenc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	counter;
+
+	counter = 0;
+	while (str[counter])
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+void	ft_putstr(char *str)
+{
+	write(1, str, ft_strlen(str));
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_params(int argc, char **argv)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 1;
+	while (i < argc -1)
+	{
+		j = 1;
+		while (j < argc - i)
+		{
+			if (ft_strcmp(argv[j], argv[j + 1]) > 0)
+			{
+				temp = argv[j];
+				argv[j] = argv[j + 1];
+				argv[j + 1] = temp;
+			}
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	if (argc <= 1)
+	{
+		return (0);
+	}
+	ft_sort_params(argc, argv);
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
+		ft_putstr("\n");
+		i++;
+	}
+	return (0);
+}
