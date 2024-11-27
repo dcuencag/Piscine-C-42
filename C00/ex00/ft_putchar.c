95fb31d7ce446cdda5ac86fae0d25d7d73c7a334
Daniel Cuenca Gutiérrez <daniel@MacBook-Air-de-Daniel.local>
2024-11-27 21:41:55 +0100

    args try out

--- C00/ex00/ft_putchar.c
+++ C00/ex00/ft_putchar.c
@@ -4,8 +4,13 @@ void ft_putchar(char c)//Creación de la función y entre parentesis creación d
 {
 	write(1, &c, 1);//Declaración de la funcion write que se ejecuta al llamar a la función original
 }
-/*
-int main(void)
+/* 
+int main(int ac, char **av)
 {
+	(void)ac;
+	(void)av;
 	ft_putchar('H');
-}*/
+	ft_putchar('\n');
+	return (0);
+}
+ */
